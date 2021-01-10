bbbfd95236b6eb2016e00ef449fe92808019a224
Aslan Tashtanov <aslantashtanov@gmail.com>
2021-01-09 20:28:19 -0500

    Added shop items
    Added some shop items. Trying things to add complexity to the game.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,83 @@
-import React from 'react';
-import Game from './Game';
-function App() {
-  return (
-    <>
-      <Game />
-      <p>$</p>
-      <button>Get money</button>
-    </>
-  )
+
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dollars: 1000,
+      employees: 0,
+      managers: 0,
+      per_second: 0
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => this.setState({
+      dollars: this.state.dollars + this.state.per_second
+    }), 1000);
+  }
+
+  IncrementItem = () => {
+    this.setState({ dollars: this.state.dollars + 1 });
+  }
+  BuyEmployee = () => {
+    if (this.state.dollars >= 25) {
+      this.setState({
+        employees: this.state.employees + 1,
+        per_second: this.state.per_second + 1,
+        dollars: this.state.dollars - 25
+      });
+    }
+  }
+  BuyManager = () => {
+    if (this.state.dollars >= 100) {
+      this.setState({
+        managers: this.state.managers + 1,
+        dollars: this.state.dollars - 100,
+        per_second: this.state.per_second + 5
+      });
+
+    }
+  }
+  render() {
+    return (
+      <>
+        <div>
+          <h2>${ this.state.dollars }</h2>
+          <h2>${ this.state.per_second }/s</h2>
+          <button onClick={this.IncrementItem}>Get money</button>
+        </div>
+        <div>
+          <h2>Shop</h2>
+          <table>
+            <tbody>
+              <tr>
+                <th>Employee: $1/s</th>
+                <th>Manager: $5/s</th>
+              </tr>
+              <tr>
+                <th>$25</th>
+                <th>$100</th>
+              </tr>
+              <tr>
+                <th><button onClick={this.BuyEmployee}>Buy</button></th>
+                <th><button onClick={this.BuyManager}>Buy</button></th>
+              </tr>
+              <tr>
+                <th>{this.state.employees}</th>
+                <th>{this.state.managers}</th>
+              </tr>
+              <tr>
+                <th></th>
+                <th></th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
